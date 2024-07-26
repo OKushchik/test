8b30e40af3b65e925634b45f6f9f81349da22f33
Oksana Kushchik <oksana.kushchyk@nixsolutions.com>
2024-07-26 13:36:01 +0200

    changes in storageUtils

--- src/utils/storageUtils.js
+++ src/utils/storageUtils.js
@@ -2,6 +2,10 @@ export const setLocalStorage = (key, value) => {
     return localStorage.setItem(key, JSON.stringify(value));
 }
 
+export const getFromLocalStorage = (key) => {
+    return localStorage.getItem(key)
+}
+
 export const getParseFromLocalStorage = (key) => {
     return JSON.parse(localStorage.getItem(key))
 }
