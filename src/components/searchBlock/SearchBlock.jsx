15477d046eccafd24144b7e1c804b6a6ed98b1a7
Oksana Kushchik <oksana.kushchyk@nixsolutions.com>
2024-07-26 10:54:54 +0200

    added validation for search field

--- src/components/searchBlock/SearchBlock.jsx
+++ src/components/searchBlock/SearchBlock.jsx
@@ -3,6 +3,7 @@ import {useRef, useState} from "react";
 
 export const SearchBlock = ({setData, isFavorite, setIsFavorite}) => {
     const [isLoaded, setIsLoaded] = useState(false)
+    const [isEmpty, setIsEmpty] = useState(false)
     const inputRef = useRef('');
     const filterDataByFavorite = () => {
         setIsFavorite(!isFavorite)
@@ -10,6 +11,7 @@ export const SearchBlock = ({setData, isFavorite, setIsFavorite}) => {
 
     const searchImages = (searchVal) => {
         if (searchVal) {
+            setIsEmpty(false)
             const URL = "https://pixabay.com/api/?key=" + import.meta.env.VITE_IMAGE_API_KEY + "&q=" + encodeURIComponent(searchVal);
             fetch(URL, {
                 method: "GET"
@@ -22,12 +24,19 @@ export const SearchBlock = ({setData, isFavorite, setIsFavorite}) => {
 
                 })
                 .catch((error) => console.log(error))
+        } else {
+            setIsEmpty(true)
         }
     }
 
     return (
         <div className={`searchBlock ${!isLoaded ? "middle" : ""}`}>
-            <input className='searchInput' ref={inputRef}/>
+            <div>
+                <input className='searchInput' ref={inputRef}/>
+                {
+                    isEmpty && <p className="searchError">please type something</p>
+                }
+            </div>
             <button className='btn searchBtn' onClick={() => searchImages(inputRef.current.value)}>Search</button>
             <button className='btn favoriteBtn'
                     onClick={filterDataByFavorite}>{isFavorite ? 'Show All' : 'My Favorite'}</button>
